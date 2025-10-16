30354176989acfe0fe5f17e5d93249392b0a5368
rsantana-tech <rogeriosantana000@gmail.com>
2025-10-16 09:55:53 -0300

    refactor login tests to use selectorList for improved readability

--- tests/cypress/first-steps-cypress/cypress/e2e/login.cy.js
+++ tests/cypress/first-steps-cypress/cypress/e2e/login.cy.js
@@ -1,22 +1,27 @@
 describe('Orange HRM Test', () => {
+  const selectorList = {
+    usernameInput: '#app [name="username"]',
+    passwordInput: '#app [name="password"]',
+    loginButton: '#app button.oxd-button',
+    dashboardGrid: '.orangehrm-dashboard-grid',
+    errorAlert: '.oxd-alert--error'
+  }
+
   it.skip('Login - sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('#app [name="username"]').click();
-    cy.get('#app [name="username"]').type('Admin');
-    cy.get('#app [name="password"]').click();
-    cy.get('#app [name="password"]').type('admin123');
-    cy.get('#app button.oxd-button').click();
+    cy.get(selectorList.usernameInput).click().type('Admin');
+    cy.get(selectorList.passwordInput).click().type('admin123');
+    cy.get(selectorList.loginButton).click();
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index');
     cy.get('.orangehrm-dashboard-grid').should('be.visible');
 
   })
   it('Login - fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    cy.get('#app [name="username"]').click();
-    cy.get('#app [name="username"]').type('test');
-    cy.get('#app [name="password"]').click();
-    cy.get('#app [name="password"]').type('test');
-    cy.get('#app button.oxd-button').click();
-    cy.get('.oxd-alert--error').should('be.visible');
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(selectorList.usernameInput).click().type('test');
+    cy.get(selectorList.passwordInput).click().type('test');
+    cy.get(selectorList.loginButton).click();
+    cy.get(selectorList.errorAlert).should('be.visible');
+    
   })
 })
