3284a57bc80953f9b7920a7fac002121721fa69c
rsantana-tech <rogeriosantana000@gmail.com>
2025-10-16 08:39:45 -0300

    adding login -fail

--- tests/cypress/first-steps-cypress/cypress/e2e/login.cy.js
+++ tests/cypress/first-steps-cypress/cypress/e2e/login.cy.js
@@ -1,10 +1,21 @@
 describe('Orange HRM Test', () => {
-  it('Login - sucess', () => {
+  it.skip('Login - sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get('#app [name="username"]').click();
     cy.get('#app [name="username"]').type('Admin');
     cy.get('#app [name="password"]').click();
     cy.get('#app [name="password"]').type('admin123');
+    cy.get('#app button.oxd-button').click();
+    cy.location('pathname').should('eq', '/web/index.php/dashboard/index');
+    cy.get('.orangehrm-dashboard-grid').should('be.visible');
+
+  })
+  it('Login - fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    cy.get('#app [name="username"]').click();
+    cy.get('#app [name="username"]').type('test');
+    cy.get('#app [name="password"]').click();
+    cy.get('#app [name="password"]').type('test');
     cy.get('#app button.oxd-button').click();
   })
 })
