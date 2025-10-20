0db1541c0d354b107582fd99ff3b43a8ba57f9b8
rsantana-tech <rogeriosantana000@gmail.com>
2025-10-20 11:31:00 -0300

    first steps with python

---
+++ python/guanabara/exercicios/challenge01.py
@@ -0,0 +1,4 @@
+# challenge 01
+# create a program that prints "Hello world!" on the screen
+msg = 'Hello, World!'
+print(msg)
---
+++ python/guanabara/exercicios/challenge02.py
@@ -0,0 +1,6 @@
+# Python Challenge 2: Write a program that reads a person's name and
+# displays a welcome message.
+
+name = input('Type your name : ')
+msg_welcome = f'Hello, Welcome {name}!'
+print(msg_welcome)
