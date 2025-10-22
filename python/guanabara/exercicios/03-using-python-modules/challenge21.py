ba04f58484c7415fe90a6d3a1c79ee3f03380362
rsantana-tech <rogeriosantana000@gmail.com>
2025-10-21 23:55:36 -0300

    play mp3

--- python/guanabara/exercicios/03-using-python-modules/challenge21.py
+++ python/guanabara/exercicios/03-using-python-modules/challenge21.py
@@ -1,6 +1,9 @@
 # Python Exercise 21: Write a Python program
 # that opens and plays the audio of an MP3 file.
 
-import
+import pygame
 
-pygame.
+pygame.init()
+pygame.mixer.music.load('my_file.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
